ce88933d2c90d7d04186d754a98fe366cb8c79be
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-08-02 22:35:12 +0100

    Updated some files

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ models = [
     linear_model.TheilSenRegressor(),
     linear_model.LinearRegression(),
 ]
+
 X, y, X_train, y_train, X_test, y_test, X_val, y_val = split_data()
 
 for model in models:
@@ -25,14 +26,6 @@ for model in models:
     time_finished = perf_counter()
     fit_time = time_start - time_finished
     clf.predict(X_train)
-    scores = cross_val_score(clf, X, y, cv=5)
-    print(model, 'cross validation score:', scores, 'time to fit:', fit_time)
-# %%
+    train_scores = cross_val_score(clf, X, y, cv=5)
+    test_scores = cross_val_score(clf, X, y, cv=5)
 
-# Report scores
-with open('metrics.txt', 'w') as outfile:
-    outfile.write("Linear Regression Metrics:\n")
-    outfile.write("Training variance explained: %2.1f%%\n" % linear_regression_test_score)
-    outfile.write("Test variance explained: %2.1f%%\n" % linear_regression_train_score)
-
-# %%
