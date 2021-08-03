18052041246833c0799fc2d8562342f4a30a54ff
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-08-03 18:17:15 +0100

    Added preprocessing of data

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from sklearn import linear_model
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler
 from get_data import split_data
 from time import perf_counter
 import numpy as np
@@ -16,13 +17,16 @@ models = [
     linear_model.TheilSenRegressor(),
     linear_model.LinearRegression(),
 ]
-
+#%%
 X, y, X_train, y_train, X_test, y_test, X_val, y_val = split_data()
+scaler = preprocessing.StandardScaler()
+X_train_scaler= scaler.fit(X_train)
+X_scaled = X_train_scaler.transform(X_train)
 
 # Create txt file of model performance
 def create_report(model):
     with open(f'{model}_metrics.txt', 'w') as outfile:
-        outfile.write(f"Regression Metrics:\nFit time: {fit_time}\n{clf} training scores: {train_scores}\n{clf} testing scores: {test_scores}\n\n")
+        outfile.write(f"Regression Metrics:\nFit time: {fit_time}\n{clf} training scores: {train_scores}\n{clf} testing scores: {test_scores}\nMean train score: {np.mean(train_scores)}\nMean test score: {np.mean(test_scores)}")
 
 for model in models:
     time_start = perf_counter()
@@ -33,5 +37,6 @@ for model in models:
     clf.predict(X_train)
     train_scores = cross_val_score(clf, X, y, cv=5)
     test_scores = cross_val_score(clf, X, y, cv=5)
-    print(f'{clf}\nTrain scores:\n{train_scores}\nTest scores: {test_scores}\nFit time: {fit_time}\n\n')
+    print(f'{clf}\nTrain scores:\n{train_scores}\nTest scores: {test_scores}\nFit time: {fit_time}\nMean train score: {np.mean(train_scores)}\nMean test score: {np.mean(test_scores)}')
     create_report(model)
+# %%
