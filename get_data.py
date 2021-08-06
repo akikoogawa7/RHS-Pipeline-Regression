85751913325bac101f3b76140e287f4a9a63a7a9
Akiko Ogawa <akiko.ogawaa@gmail.com>
2021-08-06 14:34:57 +0100

    Updating files

--- get_data.py
+++ get_data.py
@@ -4,15 +4,13 @@ import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 
-# %%
 def split_data():
     seed = 42
-    X = pd.read_csv('rhs_regression_dataset.csv')
-    y = X.pop('max ultimate height')
+    X = pd.read_csv('rhs_cleaned_dataset.csv')
+    y = X.pop('Max Time To Ultimate Height')
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=seed)
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.8, random_state=seed)
     scaler = preprocessing.StandardScaler()
     X_train_scaled = scaler.fit(X_train)
     X_train = X_train_scaled.transform(X_train)
     return X, y, X_train, y_train, X_test, y_test, X_val, y_val
-
